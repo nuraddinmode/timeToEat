addc9f98c059aace0619f9a6894722a686a20e39
Evgeniy <240025412+evgkart9@users.noreply.github.com>
2026-02-05 17:51:44 +0300

    feat: svgx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import svgx from "@svgx/vite-plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: { host: true },
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src", "assets"),
+      "@common": path.resolve(__dirname, "src", "common"),
+      "@fonts": path.resolve(__dirname, "src", "fonts"),
+      "@modules": path.resolve(__dirname, "src", "modules"),
+      "@routes": path.resolve(__dirname, "src", "routes"),
+      "@pages": path.resolve(__dirname, "src", "pages"),
+    },
+  },
+  plugins: [
+    react(),
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    svgx({
+      optimize: true,
+      svgoConfig: {
+        plugins: [
+          {
+            name: "preset-default",
+            params: { overrides: { removeViewBox: false } },
+          },
+        ],
+      },
+    }),
+  ],
+});
